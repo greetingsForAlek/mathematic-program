246e8d3c97691db3ae984ef0f2e2a75630f503a5
greetingsForAlek <alexpwoods@outlook.com>
2026-09-12 14:28:12 +0100

    IsNumber function

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -14,7 +14,22 @@ func Tokenize(data []byte) {
 		segments := strings.Split(string(line), " ")
 		
 		for _, segment := range segments {
-			fmt.Println(segment)
+			isNumber(segment)
 		}
+	}
+}
+
+func isNumber(char string) {
+	if char == "0" || 
+	char == "1" || 
+	char == "2" || 
+	char == "3" || 
+	char == "4" || 
+	char == "5" || 
+	char == "6" || 
+	char == "7" || 
+	char == "8" || 
+	char == "9" {
+		fmt.Println("char is a number.")
 	}
 }
