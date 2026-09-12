3b92c9f1d07215c6729f3ef63774cf84f52a69c9
greetingsForAlek <alexpwoods@outlook.com>
2026-09-12 15:46:56 +0100

    IsOperator Function

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -31,5 +31,22 @@ func isNumber(char string) {
 	char == "8" || 
 	char == "9" {
 		fmt.Println("char is a number.")
+	} else {
+		isOperator(char)
+	}
+}
+
+func isOperator(char string) {
+	switch char {
+	case "+":
+		fmt.Println("Addition Operator")
+	case "-":
+		fmt.Println("Subtraction Operator")
+	case "*":
+		fmt.Println("Multiplication Operator")
+	case "/":
+		fmt.Println("Division Operator")
+	case "%":
+		fmt.Println("Modulus Operator")
 	}
 }
