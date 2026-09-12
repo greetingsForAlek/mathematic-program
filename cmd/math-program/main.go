c92261145a0ee9664a4ed10f285a720c141e4531
greetingsForAlek <alexpwoods@outlook.com>
2026-09-12 10:24:53 +0100

    Read File

--- cmd/math-program/main.go
+++ cmd/math-program/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"os"
+	"fmt"
+)
+
 func main() {
-	
+	data, err := os.ReadFile("./main.mp")
+	if (err != nil) {
+		fmt.Println("Error reading file: ", err)
+	}
+
+	fmt.Println(data)
 }
