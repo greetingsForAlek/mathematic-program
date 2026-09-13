f527b563b2258fe80046d6f07c685631206842df
greetingsForAlek <alexpwoods@outlook.com>
2026-09-13 19:38:41 +0100

    Parse Line Structure

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -8,8 +8,18 @@ import (
 
 func Parse(tokens [][]lexer.Token) {
 	for _, line := range tokens {
-		for _, token := range line {
-			fmt.Println(token)
-		}
+		parseLine(line)
 	}
+}
+
+func parseLine(line []lexer.Token) error {
+	if len(line) == 0 {
+		return nil
+	}
+
+	for _, token := range line {
+		fmt.Println(token)
+	}
+
+	return nil
 }
