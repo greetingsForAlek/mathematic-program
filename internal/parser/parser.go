0009e022c533e499304613b2f3ceda82c7654db6
greetingsForAlek <alexpwoods@outlook.com>
2026-09-15 18:28:29 +0100

    Parse line and parser revamp

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -6,20 +6,56 @@ import (
 	"github.com/greetingsForAlek/MathematicProgram/internal/lexer"
 )
 
-func Parse(tokens [][]lexer.Token) {
-	for _, line := range tokens {
-		parseLine(line)
+func Parse(lines [][]lexer.Token) ([]Operation, error) {
+	operations := make([]Operation, 0, len(lines))
+
+	for _, line := range lines {
+		operation, err := parseLine(line)
+
+		if err != nil {
+			return nil, err
+		}
+
+		operations = append(operations, operation)
 	}
+
+	return operations, nil
 }
 
-func parseLine(line []lexer.Token) error {
-	if len(line) == 0 {
-		return nil
+func parseLine(line []lexer.Token) (Operation, error) {
+	if len(line) < 3 {
+		return Operation{}, fmt.Errorf("Invalid Operation.")
 	}
 
-	for _, token := range line {
-		fmt.Println(token)
+	left := Operand {
+		Value: line[0].Value,
 	}
 
-	return nil
+	if line[0].Type == lexer.PIPED {
+		left.Previous = true
+	}
+
+	operator := line[1].Value
+
+	right := Operand {
+		Value: line[2].Value,
+	}
+
+	if line[2].Type == lexer.PIPED {
+		right.Previous = true
+	}
+
+	pipe := false
+	if len(line) > 3 {
+		if line[3].Type == lexer.PIPE_RESULT {
+			pipe = true
+		}
+	}
+
+	return Operation {
+		Left: left,
+		Opertator: operator,
+		Right: right,
+		Pipe: pipe,
+	}, nil
 }
