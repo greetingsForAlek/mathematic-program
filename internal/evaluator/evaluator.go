50de441e10d7fc6c0a5d115921d7c157250b585f
greetingsForAlek <alexpwoods@outlook.com>
2026-09-15 20:02:54 +0100

    Evaluate Func

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -4,6 +4,25 @@ import (
 	"github.com/greetingsForAlek/MathematicProgram/internal/parser"
 )
 
-func Evaluate(operations []parser.Operation) {
+func Evaluate(operations []parser.Operation) ([]float64, error) {
+	results := make([]float64, 0, len(operations))
 
+	var previousResult float64
+
+	for _, operation := range operations {
+		result, err := evaluateOperation(operation, previousResult)
+
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, result)
+		previousResult = result
+	}
+
+	return results, nil
+}
+
+func evaluateOperation(operation parser.Operation, previousResult float64) (float64, error) {
+	return 10.0, nil // 10.0 is not a permanent value imma fix that in the next commit
 }
