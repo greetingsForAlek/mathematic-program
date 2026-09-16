2892b453020e35b08ce42e27011f59290e56ea7a
greetingsForAlek <alexpwoods@outlook.com>
2026-09-16 20:01:15 +0100

    Resolve Operand

--- internal/evaluator/evaluator.go
+++ internal/evaluator/evaluator.go
@@ -1,6 +1,9 @@
 package evaluator
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/greetingsForAlek/MathematicProgram/internal/parser"
 )
 
@@ -24,5 +27,18 @@ func Evaluate(operations []parser.Operation) ([]float64, error) {
 }
 
 func evaluateOperation(operation parser.Operation, previousResult float64) (float64, error) {
-	return 10.0, nil // 10.0 is not a permanent value imma fix that in the next commit
+	return 10.0, nil // i lied i need to do smt first
+}
+
+func resolveOperand(operand parser.Operand, previousResult float64) (float64, error) {
+	if operand.Previous {
+		return previousResult, nil
+	}
+
+	value, err := strconv.ParseFloat(operand.Value, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid number: %s", operand.Value)
+	}
+
+	return value, nil
 }
